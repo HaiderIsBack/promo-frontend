f526eb00e283731ee571bab00d0b7a15f351f125
Syed Zulqarnain Haider <haidershahpak17@gmail.com>
2025-10-21 17:51:09 +0500

    initial

---
+++ global.php
@@ -0,0 +1,75 @@
+<?php
+
+class Woo_Fetch {
+    private $store;
+    private $ck;
+    private $cs;
+
+    private $ch;
+
+    function __construct($store_url, $consumer_key, $consumer_secret) {
+        $this->store = $store_url;
+        $this->ck = $consumer_key;
+        $this->cs = $consumer_secret;
+
+        $this->ch = curl_init();
+
+        curl_setopt($this->ch, CURLOPT_TIMEOUT, 30);
+        
+        curl_setopt($this->ch, CURLOPT_SSL_VERIFYPEER, false);  // Or false if self-signed
+    }
+
+    function request($method, $endpoint, $params = [], $body = null) {
+        $params = array_merge($params, [
+            'consumer_key'    => $this->ck,
+            'consumer_secret' => $this->cs
+        ]);
+
+        $url = rtrim($this->store, '/') . '/' . ltrim($endpoint, '/');
+        $url .= '?' . http_build_query($params);
+
+        curl_setopt($this->ch, CURLOPT_URL, $url);
+        curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($this->ch, CURLOPT_CUSTOMREQUEST, $method);
+
+        if ($body) {
+            curl_setopt($this->ch, CURLOPT_POSTFIELDS, json_encode($body));
+            curl_setopt($this->ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+        }
+
+        $response = curl_exec($this->ch);
+        $httpCode = curl_getinfo($this->ch, CURLINFO_HTTP_CODE);
+
+        if (curl_errno($this->ch)) {
+            return ['success' => false, 'message' => curl_error($this->ch)];
+        }
+
+        if ($httpCode >= 400) {
+            return [
+                'success' => false,
+                'status' => $httpCode,
+                'message' => $response
+            ];
+        }
+
+        return [
+            'success' => true,
+            'status' => $httpCode,
+            'response' => $response
+        ];
+    }
+
+    function __destruct() {
+        if (is_resource($this->ch)) {
+            curl_close($this->ch);
+        }
+    }
+}
+
+// WooCommerce store URL (no trailing slash)
+$store_url = "https://practice-php.page.gd";
+
+// REST API credentials
+$consumer_key = "ck_cdf73c66628cad7706d9cc63c9c1e94321972f42";
+$consumer_secret = "cs_47352918dcdc3dae091c00fcab0c35bedbb387e2";
+
---
+++ product.php
@@ -0,0 +1,5 @@
+<?php
+
+if (isset($_GET['product_id'])) {
+    echo $_GET['product_id'];
+}
